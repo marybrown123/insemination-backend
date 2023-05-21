93a46db0661ade37c998a3cd70daf273393c2868
Marysia Biss <mariabiss3012@gmail.com>
2023-05-21 16:09:21 +0200

    debugging

--- controllers/bull.controller.ts
+++ controllers/bull.controller.ts
@@ -8,16 +8,16 @@ const router = express.Router();
 const bullService = new BullService();
 
 router.post("/", async (req: Request, res: Response) => {
-    // const bull: CreateBullDto = req.body;
-    // const bullFromDb = await bullService.getByName(bull.name);
-
-    // if(bullFromDb){
-    //     return res.status(400).send("Bull already exists");
-    // } 
-
-    // const newBullResponse = await bullService.create(bull);
-    // res.json(newBullResponse);
     console.log(req.body);
+    const bull: CreateBullDto = req.body;
+    const bullFromDb = await bullService.getByName(bull.name);
+
+    if(bullFromDb){
+        return res.status(400).send("Bull already exists");
+    } 
+
+    const newBullResponse = await bullService.create(bull);
+    res.json(newBullResponse);
 })
 
 router.get("/list", async (req: Request, res: Response) => {
