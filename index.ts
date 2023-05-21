7a8a8d70e9191746f438306886ce3fba7f7b161e
Marysia Biss <mariabiss3012@gmail.com>
2023-05-21 14:05:10 +0200

    add port to env

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ import routerCow from "./controllers/cow.controller"
 import routerBull from "./controllers/bull.controller"
 import routerSemen from "./controllers/semen.controller"
 import routerInsemination from "./controllers/insemination.controller"
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 app.use(express.json());
@@ -12,7 +15,7 @@ app.use("/cow", routerCow)
 app.use("/bull", routerBull)
 app.use("/semen", routerSemen)
 app.use("/insemination", routerInsemination)
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server started");
 });
 
