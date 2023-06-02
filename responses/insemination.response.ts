53044e00d7d3ee0315fabac84ed80e5b9c099bc5
Marysia Biss <mariabiss3012@gmail.com>
2023-06-02 17:21:58 +0200

    add id to insemination response

--- responses/insemination.response.ts
+++ responses/insemination.response.ts
@@ -4,10 +4,12 @@ import { SemenResponseWithBull } from "./semen.response";
 
 export class InseminationResponse {
     constructor (insemination: Insemination & {cow: Cow & {owner: Client & {adress: Adress}}} & {semen: Semen & {bull: Bull}}) {
+        this.id = insemination.id
         this.cow = new CowResponseWithOwner(insemination.cow)
         this.semen = new SemenResponseWithBull(insemination.semen)
         this.date = insemination.date;
     }
+    id: number;
     cow: CowResponseWithOwner;
     semen: SemenResponseWithBull;
     date: Date;
